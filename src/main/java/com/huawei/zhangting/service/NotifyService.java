e9169057902b497c0f5385f6e97009edf8ad1463
张挺 <zhangting@binfo-tech.com>
2020-05-23 14:21:14 +0800

    update

--- src/main/java/com/huawei/zhangting/service/NotifyService.java
+++ src/main/java/com/huawei/zhangting/service/NotifyService.java
@@ -8,7 +8,8 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -18,22 +19,30 @@ import java.util.LinkedHashMap;
  */
 @Service
 public class NotifyService {
-    private static final String MSG_URL = "https://sc.ftqq.com/SCU68613Teece1b1fd4500d64bd38230103edca2b5df23e21cab09.send?";
-    private static final String WEATHER_URL = "https://wis.qq.com/weather/common?source=xw&weather_type=forecast_24h&province=江苏&city=南京";
+
+    private static final String MSG_SEND_URL = "https://sc.ftqq.com/SCU68613Teece1b1fd4500d64bd38230103edca2b5df23e21cab09.send?";
+
+    private static final String WEATHER_FETCH_URL = "https://wis.qq.com/weather/common?source=xw&weather_type=forecast_24h";
+
+    private static final String PROVINCE = "江苏";
+
+    private static final String CITY = "南京";
+
     @Resource
     private RestTemplate restTemplate;
 
     @Scheduled(cron = "0 0 7 * * ?")
     public void excute() {
-        JSONObject weather = restTemplate.getForEntity(WEATHER_URL, JSONObject.class).getBody();
-        LinkedHashMap data = (LinkedHashMap) weather.get("data");
-        LinkedHashMap map = (LinkedHashMap) data.get("forecast_24h");
-        LinkedHashMap today = (LinkedHashMap) map.get("1");
-        String forecast = "\r\r日期:" + today.get("time") + "    \r\r地区:南京    \r\r天气:" + today.get("day_weather") + "    \r\r温度:" + today.get("min_degree") + "℃ ~ " + today.get("max_degree") + "℃";
+        JSONObject weather = restTemplate.getForEntity(WEATHER_FETCH_URL + "&province=" + PROVINCE + "&city=" + CITY, JSONObject.class).getBody();
+        Map data = (HashMap) weather.get("data");
+        Map map = (HashMap) data.get("forecast_24h");
+        Map today = (HashMap) map.get("1");
+        String forecast = "\r\r日期:" + today.get("time") + "    \r\r地区:南京    \r\r天气:" + today.get("day_weather")
+                + "    \r\r温度:" + today.get("min_degree") + "℃ ~ " + today.get("max_degree") + "℃";
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         StringBuilder msg = new StringBuilder();
         msg.append("text=天气预报提醒&desp=" + forecast + "    \r\r消息推送时间:" + format.format(new Date()));
-        String str = restTemplate.postForEntity(MSG_URL + msg.toString(), null, String.class).getBody();
+        restTemplate.postForEntity(MSG_SEND_URL + msg.toString(), null, String.class).getBody();
     }
 
 }
